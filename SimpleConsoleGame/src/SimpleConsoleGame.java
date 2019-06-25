20b97cb8f4eabcb68cbbd5c2e91be685eda85f4d
Stefano Popovski <s.popovski@mail.bg>
2019-06-25 13:46:18 +0300

    Add files via upload

---
+++ SimpleConsoleGame/src/SimpleConsoleGame.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class SimpleConsoleGame {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        int health = 100;
+        int coins = 0;
+        //INPUT
+        String[] rooms = scanner.nextLine().split("\\|");
+
+        int bestRoom = 0;
+        //LOOP UNTIL THE ROOMS ARE OVER.
+        for (int i = 0; i < rooms.length ; i++) {
+
+            //ROOM CONTENTS
+            String[] roomData = rooms[i].split("\\s+");
+            String firstToken = roomData[0];
+            //FROM STRING TO INTEGER
+            int tokenValue = Integer.parseInt(roomData[1]);
+
+            if (firstToken.equals("potion")){
+                if (health + tokenValue > 100){
+                    tokenValue = 100 - health;
+                }
+
+                health += tokenValue;
+
+                System.out.println("You healed for " + tokenValue + " hp.");
+                System.out.println("Current health " + health + " hp.");
+
+                }else if (firstToken.equals("chest")){
+                    System.out.println("You found " + tokenValue + " coins.");
+                    coins += tokenValue;
+                }else {
+                    health -= tokenValue;
+                if (health > 0){
+                    System.out.println("You slayed " + firstToken + ".");
+                }else {
+                    System.out.println("You died! Killed by " + firstToken + ".");
+                    bestRoom = i + 1;
+                    break;
+                }
+            }
+        }
+
+        if (health <= 0){
+            System.out.println("Best room: " + bestRoom);
+        }else {
+            System.out.println("You've made it!");
+            System.out.println("Coins: " + coins);
+            System.out.println("Health: " + health);
+        }
+    }
+}
